80f3131a1aa2c9203ed762df3e16351b3650d798
Youssef-Beltagy <yousseffouadbeltagy@gmail.com>
2020-12-04 09:55:31 -0800

    I made the load and copy, but they are unreliable.

--- nosqldatabase/Default.aspx.cs
+++ nosqldatabase/Default.aspx.cs
@@ -8,6 +8,8 @@ using Amazon;
 using Amazon.S3;
 using Amazon.S3.Model;
 using System.Threading.Tasks;
+using System.IO;
+
 
 // https://docs.aws.amazon.com/AmazonS3/latest/dev/CopyingObjectUsingNetSDK.html
 
@@ -17,11 +19,12 @@ namespace nosqldatabase
     public partial class _Default : Page
     {
 
-        private const string sourceBucket = "css490";
+        private const string sourceBucket = "cloud-chicken"; // Change to css490 later
         private const string destinationBucket = "cloud-chicken";
         private const string objectKey = "input.txt";
         private const string destObjectKey = "text.txt";
         private static readonly RegionEndpoint bucketRegion = RegionEndpoint.USWest2;
+        private static IAmazonS3 s3Client = new AmazonS3Client(bucketRegion);
 
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -30,7 +33,10 @@ namespace nosqldatabase
 
         protected void LoadButton_Click(object sender, EventArgs e)
         {
-            CopyingObjectAsync().Wait();
+            TextBox1.Text = "SDFS";
+
+            //CopyingObjectAsync().Wait();
+            //ReadObjectDataAsync().Wait();
 
         }
 
@@ -44,12 +50,11 @@ namespace nosqldatabase
 
         }
 
-        private static async Task CopyingObjectAsync()
+        private async Task CopyingObjectAsync()
         {
 
             try
             {
-                IAmazonS3 s3Client = new AmazonS3Client(bucketRegion);
 
                 CopyObjectRequest request = new CopyObjectRequest
                 {
@@ -58,15 +63,55 @@ namespace nosqldatabase
                     DestinationBucket = destinationBucket,
                     DestinationKey = destObjectKey
                 };
-                CopyObjectResponse response = await s3Client.CopyObjectAsync(request);
+
+                CopyObjectResponse response = await s3Client.CopyObjectAsync(request); //TODO: what is in the response?
+
+                Response.Write(response);
+
             }
             catch (AmazonS3Exception e)
             {
-                Console.WriteLine("Error encountered on server. Message:'{0}' when writing an object", e.Message);
+                Response.Write(String.Format("Error encountered on server. Message:'{0}' when copying an object", e.Message));
             }
             catch (Exception e)
             {
-                Console.WriteLine("Unknown encountered on server. Message:'{0}' when writing an object", e.Message);
+                Response.Write(String.Format("Unknown encountered on server. Message:'{0}' when copying an object", e.Message));
+            }
+        }
+
+        private static async Task ReadObjectDataAsync()
+        {
+            string responseBody = "";
+            try
+            {
+
+                GetObjectRequest request = new GetObjectRequest
+                {
+                    BucketName = sourceBucket,
+                    Key = objectKey
+                };
+
+                using (GetObjectResponse response = await s3Client.GetObjectAsync(request))
+                using (Stream responseStream = response.ResponseStream)
+                using (StreamReader reader = new StreamReader(responseStream))
+                {
+                    string title = response.Metadata["x-amz-meta-title"]; // Assume you have "title" as medata added to the object.
+                    string contentType = response.Headers["Content-Type"];
+                    Console.WriteLine("Object metadata, Title: {0}", title);
+                    Console.WriteLine("Content type: {0}", contentType);
+
+                    responseBody = reader.ReadToEnd(); // Now you process the response body.
+                    Console.WriteLine(responseBody);
+                }
+            }
+            catch (AmazonS3Exception e)
+            {
+                // If bucket or object does not exist
+                Console.WriteLine("Error encountered ***. Message:'{0}' when reading object", e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unknown encountered on server. Message:'{0}' when reading object", e.Message);
             }
         }
 
